177c7cfd1f550ddf6019dabd8087bf40af690e00
Marcus Jones <indelibleink84@gmail.com>
2019-08-21 19:25:19 -0500

    WIP: fixing errors to be able to connect buttons and pull up friendslist after using the password

--- authorize-friends/src/App.js
+++ authorize-friends/src/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Link} from 'react-router-dom'
+import {Route, Redirect} from 'react-router-dom'
 
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute'
-import FriendsList from './components/FriendsList';
 import Friends from './components/Friends'
 
+const ProtectRoute = Component => props => {
+  if (localStorage.getItem('token')) {
+    return <Component {...props} />;
+  } else{
+    return <Redirect to ='/login'/>
+  }
+}
+const ProtectedFriends = ProtectRoute(Friends)
 function App() {
   return (
    
       <div className='App'>
-        <Login />
-        <ul>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-          <li>
-            <Link to='/friends'>Friends</Link>
-          </li> 
-        </ul>
-        <PrivateRoute exact path= '/protected' 
-        component={FriendsList} />
-        <PrivateRoute exact path= 'friends'
+        <Route path= '/login' 
+        component={Login} />
+        <PrivateRoute path= '/friends'
         component={Friends} />
+        {/* <Route path='/friends' component={ProtectedFriends}/> */}
       </div>
   );
 }
--- authorize-friends/src/components/Login.js
+++ authorize-friends/src/components/Login.js
@@ -56,7 +56,7 @@ class Login extends React.Component {
                     {/* {touched.password && errors.password && (
                         <p className = 'error'>{errors.password}</p>
                     )} */}
-                    <button type='submit'>Enter</button>
+                    <button type='submit'>Add Friend</button>
                 </form>
             </div>
         )
